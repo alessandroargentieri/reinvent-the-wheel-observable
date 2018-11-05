6ba70846b596d4ef337ed7e45f7eda734fcc1c4a
Alessandro Argentieri <alexmawashi87@gmail.com>
2018-11-05 18:10:08 +0100

    implemented map method

--- src/main/java/com/quicktutorialz/observable/MainApplication.java
+++ src/main/java/com/quicktutorialz/observable/MainApplication.java
@@ -6,10 +6,22 @@ public class MainApplication {
 
     public static void main(String[] args){
         String text = "This is a nice text to be processed reactively";
-        new Observable().just(() -> processText(text)).subscribe(obj -> System.out.println(obj));
+        //new Observable().just(() -> processText(text)).subscribe(obj -> System.out.println(obj));
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        new Observable().just(() -> processText(text))
+                        .map(t -> processText((String) t))
+                        .map(t -> processText((String) t))
+                        .map(t -> processTextObs((String) t))
+                        .map(t -> processText((String) t))
+                        .subscribe(result -> System.out.println(result));
+
     }
 
     private static String processText(String text){
         return text + " and you must believe it! ";
+    }
+
+    private static Observable processTextObs(String text){
+        return new Observable().just(() -> text + "YOU MUST BELIEVE IT! ");
     }
 }
--- src/main/java/com/quicktutorialz/observable/observable/Observable.java
+++ src/main/java/com/quicktutorialz/observable/observable/Observable.java
@@ -1,11 +1,15 @@
 package com.quicktutorialz.observable.observable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class Observable {
 
     private Supplier supplier;
+    private List<Function> mapList = new ArrayList<>();
 
     public Observable just(Supplier supplier){
         this.supplier = supplier;
@@ -13,14 +17,45 @@ public class Observable {
     }
 
     public void subscribe(Consumer onFinish){
-        onFinish.accept(supplier.get());
+        onFinish.accept(getResult());
     }
 
     public void subscribe(Consumer onFinish, Consumer onError){
         try{
-            onFinish.accept(supplier.get());
+            onFinish.accept(getResult());
         }catch(Exception e){
             onError.accept(e);
+        }
+    }
+
+    public Observable map(Function function){
+        mapList.add(function);
+        return this;
+    }
+
+
+    private Object getResult(){
+        Object result = supplier.get();
+        for(Function f : mapList){
+            result = f.apply(result);
+            if(result instanceof Observable){
+                PartialResult partial = new PartialResult();
+                ((Observable) result).subscribe(res -> partial.setResult(res));
+                result = partial.getResult();
+            }
+        }
+        return result;
+    }
+
+    class PartialResult {
+        Object result;
+
+        public Object getResult() {
+            return result;
+        }
+
+        public void setResult(Object result) {
+            this.result = result;
         }
     }
 
